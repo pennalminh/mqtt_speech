63182ad5e5aa4259f3e20b6c733a2b1fdc4584d9
pennalminh <pennalminh@gmail.com>
2025-06-28 12:48:21 +0700

    add cancel and getout

--- mqtt_client.py
+++ mqtt_client.py
@@ -58,23 +58,29 @@ def on_message(client, userdata, msg):
     print(message_json)
 
     if message_json["text"] == "monitor_1":
-        repeat_thread = threading.Thread(target=repeat_play, args=("monitor_1.mp3",5))
+        repeat_thread = threading.Thread(target=repeat_play, args=("monitor_1.mp3",10))
         repeat_thread.start()
+    elif message_json["text"] == "monitor_3":
+        repeat_thread = threading.Thread(target=repeat_play, args=("monitor_3.mp3",10))
+        repeat_thread.start()    
     elif message_json["text"] == "monitor_4":
-        repeat_thread = threading.Thread(target=repeat_play, args=("monitor_4.mp3",5))
+        repeat_thread = threading.Thread(target=repeat_play, args=("monitor_4.mp3",10))
+        repeat_thread.start()
+    elif message_json["text"] == "monitor_6":
+        repeat_thread = threading.Thread(target=repeat_play, args=("monitor_6.mp3",10))
+        repeat_thread.start()
+    elif message_json["text"] == "get_out":
+        repeat_thread = threading.Thread(target=repeat_play, args=("get_out.mp3",10))
         repeat_thread.start()
     elif "isRepeat" in message_json and message_json["isRepeat"] == True:
-        
         repeat_thread = threading.Thread(target=repeat_play, args=(message_json["text"],3, False))
         repeat_thread.start()
     else:
-        # Nếu không phải monitor_3 hoặc monitor_4, thì phát 1 lần
         switch_message = {
             'monitor_2': lambda: subprocess.run(["mpv", "monitor_2.mp3"]),
-            'monitor_3': lambda: subprocess.run(["mpv", "monitor_3.mp3"]),
-            'monitor_6': lambda: subprocess.run(["mpv", "monitor_6.mp3"]),
             'status_3': lambda: subprocess.run(["mpv", "status_3.mp3"]),
             'status_4': lambda: subprocess.run(["mpv", "status_4.mp3"]),
+            'cancel': lambda: subprocess.run(["mpv", "cancel.mp3"]),
         }
         action = switch_message.get(message_json["text"], lambda: play_text_with_edge(message_json["text"]))
         action()
